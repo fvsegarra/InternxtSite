f59c131cf216070098974bea5f92dcd557b02b47
Joe Priest <joe.priest@live.co.uk>
2018-05-21 16:59:54 +0100

    add Privacy Policy page

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,55 @@
+@extends('_layouts.master')
+
+@section('pageID', 'terms')
+
+@section('navbar-classes', 'navbar-light')
+
+@section('pageTitle', 'Privacy Policy')
+
+@section('body')
+
+	<header class="pageHeader">
+		<div class="container">
+			<h1 class="pageHeader__heading">Privacy Policy</h1>
+		</div>
+	</header>
+
+	<section class="section section--terms-overview">
+		<div class="container">
+			<p class="section__heading">Overview</p>
+			<div class="section__content">
+				<p>Internxt Universal Technologies SLU ("Internxt") deeply cares about ensuring the fundamental personal data protection rights of all of its users, clients, and anyone interested in the company by adhering to current personal data protection regulations.</p>
+
+				<p>This Privacy Policy must be accepted by clients before any of our products can be purchased. This is because our organization needs to treat your data in a personal way. However, this is always done in compliance with current personal data protection regulations. Therefore, when you register on our website, only the necessary data required to carry out the service will be collected, thus complying with the stipulations set out in Organic Law 15/1999 of December 13 on personal data protection and in Royal Decree 1720/2007 of December 21, which approves the implementation of these regulations. Users, by accepting this Privacy Policy, declare that all data provided to create an account is accurate and up to date. Users must contact Internxt if they wish to modify any personal data – or they can edit their data in the relevant section of our website.</p>
+
+				<p>Users are responsible for any false or inaccurate statements or information provided to Internxt as well as any harm or damage caused to the company by said statements or information. We also reiterate Internxt' commitment to scrupulously comply with every provision set out in current regulations regarding personal data protection. And, specifically, Internxt is committed to the obligation of keeping secret the data to which it has access and to take the necessary security measures to prevent access, modification or deletion of data in an unauthorized manner. </p>
+
+				<p>This Privacy Policy may be modified at the discretion of Internxt after informing of any change on our website. This enables us to maintain criteria of information transparency and fairness with regard to the treatment that governs the actions of our organization regarding the processing of personal data. According to the provisions set out in Organic Law 15/1999 of December 13 on Personal Data Protection, you are hereby informed that this data will be included in a file owned by Internxt and will be used to manage Internxt' relationship with its users. You are also informed that you have the right to access, rectify, and cancel data if necessary, and to object to its processing under the terms established in the aforementioned Organic Law 15/1999. These rights may be exercised at Internxt' official address for this purpose: Calle San Vicente Martir 85, 46007, Valencia, Spain.</p>
+			</div>{{-- /.section__content --}}
+		</div>
+	</section>
+
+	<section class="section section--terms-website">
+		<div class="container">
+			<p class="section__heading">Personal data privacy and form-based collection.</p>
+			<div class="section__content">
+				<p>In accordance with prevailing laws and regulations on personal data protection, we notify you that your data will be entered in a processing system owned by Internxt Universal Technologies SLU, holding tax identification number B98936354 and having its registered office at Calle San Vicente Martir 85, 46007, Valencia, Spain, for the purpose of replying to your queries and sending you information that may be of interest to you.</p>
+
+				<p>In compliance with prevailing laws and regulations, Internxt gives notice that the data will be kept for the time strictly required to comply with the statutory provisions referred to above. Unless you tell us otherwise, we shall assume that your data remained unchanged. You undertake to tell us about any change and give your consent to our using the data for the purposes referred to above. Internxt states that it will process the data legally, fairly, transparently, appropriately, relevantly, subject to limits, accurately and ensure that all data are up to date. Internxt, therefore, undertakes to take all reasonable steps for data to be erased or rectified without delay if they are inaccurate. In accordance with the rights conferred on you by prevailing laws and regulations on data protection, you may exercise your rights of access, rectification, limitation of processing, erasure, portability and objection to processing of your personal data, and surrounding the consent given to their processing, by writing to hello@internxt.com.</p>
+
+				<p>The customer acknowledges and accepts that in the event of a technical issue, exceptionally our engineers might have to access the physical server in which the customer data is store and by doing so in very rare circumstances might access customer data. Should this be the case, our engineers will limit their activities to the fixing of the technical problems without using the access or the data that they might have access to for any other uses. Internxt also informs that according to the Spanish Law 25/2007 of 18th of October 2007, that regulates the electronic data communications, Internxt may have to store and retain certain information for the access of the relevant and legitimate public authorities.</p>
+
+			</div>{{-- /.section__content --}}
+		</div>
+	</section>
+
+	<section class="section section--terms-">
+		<div class="container">
+			<p class="section__heading">Contact us</p>
+			<div class="section__content">
+				<p>If you have any other concerns, suggestions or questions about any issues relating to privacy on any of Internxt online properties, please contact us via e-mail (hello@internxt.com).</p>
+			</div>{{-- /.section__content --}}
+		</div>
+	</section>
+
+@endsection
