7a9d8ce5bcb299b639faa91c5f87de5a62a28c39
Joe Priest <joe.priest@live.co.uk>
2018-05-25 14:13:21 +0100

    add links from previews to X Core and Cloud pages

--- resources/views/_includes/previews/xcloud.blade.php
+++ resources/views/_includes/previews/xcloud.blade.php
@@ -1,4 +1,4 @@
-<section class="section section--preview-xcloud">
+<a href="{{ route('cloud') }}" class="section section--preview-xcloud">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm offset-lg-6">
@@ -9,4 +9,4 @@
 			</div>
 		</div>
 	</div>
-</section>
+</a>
