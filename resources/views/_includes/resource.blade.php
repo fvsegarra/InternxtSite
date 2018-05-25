dfe77a0ed7da74a618f74a7f78804c2cd31e8ed2
Joe Priest <joe.priest@live.co.uk>
2018-05-25 16:13:03 +0100

    ensure separators on Roadmap page extend to full page width

--- resources/views/_includes/resource.blade.php
+++ resources/views/_includes/resource.blade.php
@@ -1,14 +1,16 @@
-<div class="subsection subsection--{{ $class }}">
-	<p class="subsection__heading">{{ $title }}</p>
-	<div class="row">
+<div class="container">
+	<div class="subsection subsection--{{ $class }}">
+		<p class="subsection__heading">{{ $title }}</p>
+		<div class="row">
 
-		<div class="col-lg-8">
-			<p class="subsection__content">{{ $content }}</p>
-		</div>
+			<div class="col-lg-8">
+				<p class="subsection__content">{{ $content }}</p>
+			</div>
 
-		<div class="col-lg">
-			<a class="subsection__btn" href="{{ $btnLink }}">{{ $btnText }}</a>
-		</div>
+			<div class="col-lg">
+				<a class="subsection__btn" href="{{ $btnLink }}">{{ $btnText }}</a>
+			</div>
 
-	</div>{{-- /.row --}}
+		</div>{{-- /.row --}}
 </div>{{-- /.subsection --}}
+</div>
--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -9,34 +9,34 @@
 @section('body')
 
 	<section class="section section--resources">
-		<div class="container-fluid">
+		<div class="container">
 			<p class="section__heading">Company Resources.</p>
-			<div class="section__content">
-
-				@component('_includes.resource')
-					@slot('class', 'press-kit')
-					@slot('title', 'Press Kit')
-					@slot('btnLink', '#download')
-					@slot('btnText', 'Download Press Kit')
-					@slot('content')
-						Download our press kit for a selection of our assets to use for any media or marketing purposes.
-					@endslot
-				@endcomponent
-
-				<hr>
-
-				@component('_includes.resource')
-					@slot('class', 'whitepaper')
-					@slot('title', 'Whitepaper')
-					@slot('btnLink', '#download')
-					@slot('btnText', 'Download Whitepaper')
-					@slot('content')
-						Download our whitepaper for an extensive look into the technology we are applying to our services.
-					@endslot
-				@endcomponent
-
-			</div>{{-- /.section__content --}}
 		</div>
+		<div class="section__content">
+
+			@component('_includes.resource')
+				@slot('class', 'press-kit')
+				@slot('title', 'Press Kit')
+				@slot('btnLink', '#download')
+				@slot('btnText', 'Download Press Kit')
+				@slot('content')
+					Download our press kit for a selection of our assets to use for any media or marketing purposes.
+				@endslot
+			@endcomponent
+
+			<hr>
+
+			@component('_includes.resource')
+				@slot('class', 'whitepaper')
+				@slot('title', 'Whitepaper')
+				@slot('btnLink', '#download')
+				@slot('btnText', 'Download Whitepaper')
+				@slot('content')
+					Download our whitepaper for an extensive look into the technology we are applying to our services.
+				@endslot
+			@endcomponent
+
+		</div>{{-- /.section__content --}}
 	</section>
 
 @endsection
