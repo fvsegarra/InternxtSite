5e2443823c0fdb5722cc08c228b6a4aa0109c1d7
Joe Priest <joe.priest@live.co.uk>
2018-05-25 12:11:21 +0100

    override breakpoint for offset column in X Cloud preview

--- resources/views/_includes/previews/xcloud.blade.php
+++ resources/views/_includes/previews/xcloud.blade.php
@@ -1,7 +1,7 @@
 <section class="section section--preview-xcloud">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm offset-md-6">
+			<div class="col-sm offset-lg-6">
 				<img class="section__icon" src="/img/logos/xcloud.svg" height="75">
 				<p class="section__label">X Cloud</p>
 				<p class="section__heading">A Step-forward for the cloud.</p>
