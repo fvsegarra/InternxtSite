3f2ff02818179074e61a4e1f28f1a466b76ee33f
Joe Priest <joe.priest@live.co.uk>
2018-05-25 15:24:17 +0100

    improve display of team page

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -15,94 +15,94 @@
 
 				<div class="row">
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'fran')
 							@slot('name', 'Fran Villalba Segerra')
 							@slot('position', 'Founder & Chief Executive Officer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'volodymyr')
 							@slot('name', 'Volodymyr Shevchyk')
 							@slot('position', 'Chief Technology Officer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'jordan')
 							@slot('name', 'Jordan Latham')
 							@slot('position', 'Chief Design Officer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'zoran')
 							@slot('name', 'Zoran Joka')
 							@slot('position', 'Senior Software Developer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'russell')
 							@slot('name', 'Russell Vaughan')
 							@slot('position', 'Business Development')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'conor')
 							@slot('name', 'Conor Doyle')
 							@slot('position', 'Chief Marketing Officer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'harrison')
 							@slot('name', 'Harrison Moore')
 							@slot('position', 'UI/UX Designer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'igor')
 							@slot('name', 'Igor Dostanic')
 							@slot('position', 'Software Developer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'joe')
 							@slot('name', 'Joe Priest')
 							@slot('position', 'Front-End Developer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'alberto')
 							@slot('name', 'Alberto Pinero')
 							@slot('position', 'Hardware Engineer')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team-blank')
 							@slot('name', 'Join the team')
 							@slot('position', 'View our open job positions')
 							@slot('btnText', 'View current openings')
 							@slot('btnLink', 'https://angel.co/internxt/jobs')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
 				</div>{{-- /.row --}}
 			</div>
@@ -118,30 +118,30 @@
 
 				<div class="row">
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'alex')
 							@slot('name', 'Alex Sicart Ramos')
 							@slot('position', 'Technical Advisor')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team')
 							@slot('slug', 'ryan')
 							@slot('name', 'Ryan Marsden')
 							@slot('position', 'Financial Advisor')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team-blank')
 							@slot('name', 'Become an advisor')
 							@slot('position', 'Contact us about an enquiry.')
 							@slot('btnText', 'Contact us')
 							@slot('btnLink', 'mailto:hello@internxt.com')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
 				</div>{{-- /.row --}}
 
@@ -159,21 +159,21 @@
 
 				<div class="row">
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team-partner')
 							@slot('slug', 'civic')
 							@slot('name', 'Civic')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
-					<div class="col-sm-4">
+					<div class="col-lg-4">
 						@component('_includes.team-blank')
 							@slot('name', 'Become a partner')
 							@slot('position', 'Contact us about an enquiry.')
 							@slot('btnText', 'Contact us')
 							@slot('btnLink', 'mailto:hello@internxt.com')
 						@endcomponent
-					</div>{{-- /.col-sm-4 --}}
+					</div>{{-- /.col-lg-4 --}}
 
 				</div>{{-- /.row --}}
 
