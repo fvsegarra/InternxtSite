e76ea761451389f272f451e344e01f7ae92c3897
Joe Priest <joe.priest@live.co.uk>
2018-06-02 12:15:47 +0100

    upload new version of civic test

--- resources/views/civic-test.blade.php
+++ resources/views/civic-test.blade.php
@@ -5,19 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/civic/civic-modal.min.css">
-    <script src="/js/civic/civic.sip.min.js"></script>
-    <script src="/js/civic/jquery-3.3.1.slim.min.js"></script>
+    <link rel="stylesheet" href="https://hosted-sip.civic.com/css/civic-modal.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://hosted-sip.civic.com/js/civic.sip.min.js"></script>
     <script src="https://raw.githubusercontent.com/emn178/js-sha256/master/build/sha256.min.js"></script>
     <script type="text/javascript" src="qrc:///qtwebchannel/qwebchannel.js"></script>
     <title>Civic test</title>
+    <section id='token' hidden></section>
+
+
 </head>
 
 <body>
     <script>
 
         $(document).ready(function() {
-	       var socket = new WebSocket("wss://localhost:6382");
             var civicSip = new civic.sip({
                 appId: 'Skzcny80G'
             });
@@ -30,14 +32,7 @@
             civicSip.on('auth-code-received', function (event) {
                 console.log('auth-code-received', event.response);
                 window.civicToken = event.response;
-
-    	        //send data to server
-    	        new QWebChannel(socket, function(channel) {
-    	            // make core object accessible globally
-                    window.core = channel.objects.core;
-        		    console.log("Send token to user...");
-        		    core.receiveText(window.civicToken);
-                });
+            	token.innerText += window.civicToken
             });
 
             civicSip.on('civic-sip-error', function (error) {
