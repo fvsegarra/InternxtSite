92ff1ea0f76092139ad3c8c43855d9013a98a4f5
Joe Priest <joe.priest@live.co.uk>
2018-08-20 17:54:44 +0100

    add background images to X Core and X Cloud preview sections on larger viewports

--- resources/views/_includes/previews/xcloud.blade.php
+++ resources/views/_includes/previews/xcloud.blade.php
@@ -1,11 +1,9 @@
 <div href="{{ route('cloud') }}" class="section section--preview-xcloud">
-	<div class="container">
-		<div class="section__content">
-			<img class="section__icon" src="/img/logos/xcloud.png" height="72">
-			<p class="section__label">X Cloud</p>
-			<p class="section__heading">Secure cloud storage.</p>
-			<p class="section__subheading">A cloud platform built with total security and privacy.</p>
-			<a class="section__cta">Learn more</a>
-		</div>{{-- /.section__content --}}
-	</div>
+	<div class="section__content">
+		<img class="section__icon" src="/img/logos/xcloud.png" height="72">
+		<p class="section__label">X Cloud</p>
+		<p class="section__heading">Secure cloud storage.</p>
+		<p class="section__subheading">A cloud platform built with total security and privacy.</p>
+		<a class="section__cta">Learn more</a>
+	</div>{{-- /.section__content --}}
 </div>
--- resources/views/_includes/previews/xcore.blade.php
+++ resources/views/_includes/previews/xcore.blade.php
@@ -1,11 +1,9 @@
 <div href="{{ route('core') }}" class="section section--preview-xcore">
-	<div class="container">
-		<div class="section__content">
-			<img class="section__icon" src="/img/logos/xcore.png" height="75">
-			<p class="section__label">X Core</p>
-			<p class="section__heading">Get paid to share.</p>
-			<p class="section__subheading">Enabling hosts to store and transfer data.</p>
-			<a class="section__cta">Learn more &gt;</a>
-		</div>{{-- /.section__content --}}
-	</div>
+	<div class="section__content">
+		<img class="section__icon" src="/img/logos/xcore.png" height="75">
+		<p class="section__label">X Core</p>
+		<p class="section__heading">Get paid to share.</p>
+		<p class="section__subheading">Enabling hosts to store and transfer data.</p>
+		<a class="section__cta">Learn more &gt;</a>
+	</div>{{-- /.section__content --}}
 </div>
