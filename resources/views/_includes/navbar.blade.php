e434008d60d0493e2fbd89f67458e4a18289a8ee
Joe Priest <joe.priest@live.co.uk>
2018-09-09 23:50:18 +0100

    WIP on new design of navbar dropdown menus

--- resources/views/_includes/navbar.blade.php
+++ resources/views/_includes/navbar.blade.php
@@ -12,19 +12,59 @@
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Products
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('cloud') }}">X Cloud</a>
-                        <a class="dropdown-item" href="{{ route('core') }}">X Core</a>
+                    <div class="dropdown-menu dropdown-menu--products" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item dropdown-item--xcloud" href="{{ route('cloud') }}">
+                            <div class="media">
+                                <img class="dropdown-item__image align-self-center" src="/img/logos/xcloud.svg" alt="Placeholder">
+                                <div class="media-body">
+                                    <h5 class="dropdown-item__heading">X Cloud</h5>
+                                    <p class="dropdown-item__subheading">Secure cloud storage.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="dropdown-item dropdown-item--xcore" href="{{ route('core') }}">
+                            <div class="media">
+                                <img class="dropdown-item__image align-self-center" src="/img/logos/xcore.png" alt="Placeholder">
+                                <div class="media-body">
+                                    <h5 class="dropdown-item__heading">X Core</h5>
+                                    <p class="dropdown-item__subheading">Get paid to share.</p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Company
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('about') }}">About us</a>
-                        <a class="dropdown-item" href="{{ route('roadmap') }}">Roadmap</a>
-                        <a class="dropdown-item" href="{{ route('resources') }}">Resources</a>
+                    <div class="dropdown-menu dropdown-menu--company" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item dropdown-item" href="{{ route('about') }}">
+                            <div class="media">
+                                <img class="dropdown-item__image align-self-center" src="//placehold.it/28x28" alt="Placeholder">
+                                <div class="media-body">
+                                    <h5 class="dropdown-item__heading">About us</h5>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="dropdown-item dropdown-item" href="{{ route('roadmap') }}">
+                            <div class="media">
+                                <img class="dropdown-item__image align-self-center" src="//placehold.it/28x28" alt="Placeholder">
+                                <div class="media-body">
+                                    <h5 class="dropdown-item__heading">Roadmap</h5>
+                                </div>
+                            </div>
+                        </a>
+                        <hr>
+                        <a class="dropdown-item dropdown-item" href="{{ route('resources') }}">
+                            <div class="media">
+                                <img class="dropdown-item__image align-self-center" src="//placehold.it/28x28" alt="Placeholder">
+                                <div class="media-body">
+                                    <h5 class="dropdown-item__heading">Resources</h5>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </li>
                 <li class="nav-item">
