8ea4b0a7f1a04e1fdd84d11ff06649129437cb9e
Joe Priest <joe.priest@live.co.uk>
2018-09-16 19:01:29 +0100

    add container to X Core preview

--- resources/views/_includes/previews/xcore.blade.php
+++ resources/views/_includes/previews/xcore.blade.php
@@ -1,9 +1,11 @@
 <div href="{{ route('core') }}" class="section section--preview-xcore">
-	<div class="section__content">
-		<img class="section__icon" src="/img/logos/xcore.png" height="75">
-		<p class="section__label">X Core</p>
-		<p class="section__heading">Get paid to share.</p>
-		<p class="section__subheading">Enabling hosts to store and transfer data.</p>
-		<a class="section__cta" href="{{ route('core') }}">Learn more</a>
-	</div>{{-- /.section__content --}}
+	<div class="container">
+		<div class="section__content">
+			<img class="section__icon" src="/img/logos/xcore.png" height="75">
+			<p class="section__label">X Core</p>
+			<p class="section__heading">Get paid to share.</p>
+			<p class="section__subheading">Enabling hosts to store and transfer data.</p>
+			<a class="section__cta" href="{{ route('core') }}">Learn more</a>
+		</div>{{-- /.section__content --}}
+	</div>{{-- /.container --}}
 </div>
