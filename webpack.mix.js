43707641e2662452990960f5b1acd4b341a514b7
Joe Priest <joe.priest@live.co.uk>
2018-09-18 17:47:48 +0100

    add versioning to css files to make debugging a bit easier

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .browserSync('local.internxt.uk')
+   .version()
 ;
