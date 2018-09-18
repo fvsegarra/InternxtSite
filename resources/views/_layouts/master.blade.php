65845248b2df2be0bb08f1df67ee3c73b56c55fc
Joe Priest <joe.priest@live.co.uk>
2018-09-18 16:40:57 +0100

    add cookie consent banner

--- resources/views/_layouts/master.blade.php
+++ resources/views/_layouts/master.blade.php
@@ -24,6 +24,30 @@
 
         @endif
 
+        <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+        <script>
+        window.addEventListener("load", function(){
+        window.cookieconsent.initialise({
+          "palette": {
+            "popup": {
+              "background": "#ffffff",
+              "text": "#000000"
+            },
+            "button": {
+              "background": "#000000",
+              "text": "#ffffff"
+            }
+          },
+          "theme": "edgeless",
+          "content": {
+            "message": "We use cookies to ensure you get the best experience on our website.",
+            "dismiss": "",
+            "link": ""
+          }
+        })});
+        </script>
+
     </head>
     <body id="page-@yield('pageID')">
 
