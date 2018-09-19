60b26318e4126faef5e1c73a518202b876cc1199
Joe Priest <joe.priest@live.co.uk>
2018-09-19 12:14:17 +0100

    replace old cookie consent banner

--- resources/views/_includes/scripts/cookie-consent.blade.php
+++ resources/views/_includes/scripts/cookie-consent.blade.php
@@ -1,18 +1,23 @@
-<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
 <script>
     window.addEventListener("load", function(){
     window.cookieconsent.initialise({
-        "palette": {
-            "popup": {
-                "background": "#000000",
-                "text": "#ffffff"
-            },
-            "button": {
-                "background": "#ffffff",
-                "text": "#000000"
-            }
+      "palette": {
+        "popup": {
+          "background": "#ffffff",
+          "text": "#000000"
         },
-        "showLink": false
+        "button": {
+          "background": "#000000",
+          "text": "#ffffff"
+        }
+      },
+      "theme": "edgeless",
+      "content": {
+        "message": "We use cookies to ensure you get the best experience on our website.",
+        "dismiss": "",
+        "link": ""
+      }
     })});
 </script>
