0f2b6780c8e98dd483a5aa82a2b8a7544471d9cd
Joe Priest <joe.priest@live.co.uk>
2018-09-26 09:23:48 +0100

    use two versions of X Cloud Beta button at bottom of page to prevent modal showing on smaller viewports

--- resources/views/cloud.blade.php
+++ resources/views/cloud.blade.php
@@ -264,9 +264,8 @@
 			<div class="section__outline d-lg-flex align-items-center justify-content-between">
 				<img class="section__icon" src="/img/logos/xcloud-flat.jpg" height="39">
 				<p class="section__heading">Jump into X Cloud Beta today.</p>
-				<button class="section__cta btn btn-primary" type="button" data-toggle="modal" data-target="#modalXCloud">
-					Get X Cloud Beta
-				</button>
+				<a href="#beta" class="section__cta d-lg-none">Get X Cloud Beta</a>
+				<a href="#beta" class="section__cta d-none d-lg-block" data-toggle="modal" data-target="#modalXCloud">Get X Cloud Beta</a>
 			</div>{{-- /.section__outline --}}
 		</div>
 	</section>
