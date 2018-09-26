33fd1bce45d0f88f8a245f29eb906a26883b7d36
Joe Priest <joe.priest@live.co.uk>
2018-09-26 13:38:42 +0100

    move cookie consent styles to separate stylesheet

--- resources/views/_includes/scripts/cookie-consent.blade.php
+++ resources/views/_includes/scripts/cookie-consent.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<link rel="stylesheet" type="text/css" href="/css/cookie-consent.css">
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
 <script>
     window.addEventListener("load", function(){
