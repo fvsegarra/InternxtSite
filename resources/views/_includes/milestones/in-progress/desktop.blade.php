92c045e7ad8dd76ed57b5cb89c2e849c93943d27
Joe Priest <joe@infusion-it.co.uk>
2018-09-28 23:09:01 +0000

    add changes from server (automatic commit)

--- resources/views/_includes/milestones/in-progress/desktop.blade.php
+++ resources/views/_includes/milestones/in-progress/desktop.blade.php
@@ -11,8 +11,8 @@
 		@endcomponent
 
 		@component('_includes.milestone')
-			@slot('date', 'Q3 2019')
-			@slot('title', 'Next Service')
+			@slot('date', 'Q2 2019')
+			@slot('title', 'CorePro')
 			@slot('content')
 				After the public release of X Cloud and X Core, we will then proceed with the development of our next service.
 			@endslot
@@ -20,7 +20,18 @@
 
 	</div>{{-- /.col-lg-4 --}}
 
+	<div class="col-lg-4">
 
+		@component('_includes.milestone')
+			@slot('date', 'Q3 2019')
+			@slot('title', 'Next service')
+			@slot('content')
+			A highly optimized, easy-to-use storage server which enables hosts to maximize their profits as an X Core Host.
+			@endslot
+		@endcomponent
+
+
+	</div>{{-- /.col-lg-4 --}}
 
 
 	<div class="col-lg-4">
