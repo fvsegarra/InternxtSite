566ccfdbe03d3700266ea070a61e4b0db24ebe6d
Joe Priest <joe@infusion-it.co.uk>
2018-11-21 10:50:01 +0000

    add changes from server (automatic commit)

--- resources/views/token.blade.php
+++ resources/views/token.blade.php
@@ -195,31 +195,7 @@
 	</script>
 	 </div>
 	</section>
-	<section class="section section--subscribe">
-	<div class="container">
-	 <div class="section__border"></div>
-	 <div class="row">
-	  <div class="col-lg-6">
-	   <p class="section__label">Keep Updated</p>
-	   <h2 class="section__heading">Subscribe to our mailing list.</h2>
-	   <p class="section__subheading">Get the latest Internxt news and offers directly to your mailbox.</p>
-	  </div>
-	  <div class="col-lg-5">
-	   <form class="section--subscribe__form ml-block-form" action="https://app.mailerlite.com/webforms/submit/r3s4c1" data-code="r3s4c1" method="post" target="_blank">
-	    <input type="hidden" name="ml-submit" value="1">
-	    <div class="row">
-	     <div class="col-lg col-lg--input">
-	      <input type="email" class="section--subscribe__input" name="fields[email]" value="" placeholder="Email" required>
-	     </div>
-	     <div class="col-lg-3 col-lg--btn">
-	      <button type="submit" class="section--subscribe__btn primary">Subscribe</button>
-	     </div>
-	    </div>
-	   </form>
-	  </div>
-	 </div>
-	</div>
-	</section>
+
 
 	@include('_includes.sections.subscribe')
 
