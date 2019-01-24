bfb1024c97753c63f0208a7c344871593e5790ee
Joe Priest <joe@infusion-it.co.uk>
2019-01-24 19:02:01 +0000

    add changes from server (automatic commit)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -63,7 +63,7 @@
 							@slot('name', 'Fran Villalba Segarra')
 							@slot('role', 'Founder & CEO')
 							@slot('bio')
-								Fran is the architect behind Internxt. He learned to code at an early age and was soon awarded an internship at OnePlus, a smartphone manufacturer serving 38 different countries. He then went onto work at Hostinger one of the worlds largest web hosting companies. Fran then began his journey into entrepreneurship by launching his first startup OneSite, a platform which allows anyone to create their own website for free, which had over 10,000 users within its first few months. Fran’s listed on The Next Web's T500, which presents the 500 most talented in the digital scene.
+								Fran is the architect behind Internxt. He learned to code at an early age and was soon awarded an internship at OnePlus, a smartphone manufacturer serving 38 different countries. He then went onto work at Hostinger one of the worlds largest web hosting companies. Fran then began his journey into entrepreneurship by launching his first startup OneSite, a platform which allows anyone to create their own website for free, which had over 10,000 users within its first few months. Fran’s listed on The Next Web's T500, which presents the 500 most talented in the digital scene. He was also nominated for Forbes 30 Under 30 2017.
 							@endslot
 						@endcomponent
 					</div>{{-- /.col-lg-3 --}}
@@ -116,7 +116,7 @@ Jesus has a BSc in Computer Science, a Master's Degree in Decentralized Computin
 						@component('_includes.team')
 							@slot('slug', 'joe')
 							@slot('name', 'Joe Priest')
-							@slot('role', 'Front-End Developer')
+							@slot('role', 'Software Developer')
 							@slot('bio')
 								Joe has a Bachelors degree in Computer Science and extensive knowledge of full-stack web development, with over 15 years experience in the field. He is responsible for translating the UI/UX designs into code that will produce the elements of Internxt’s website, applications and services that you will see and interact with.
 							@endslot
