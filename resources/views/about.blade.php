eb53251eceb93299582da0f156098a018315a97f
Joe Priest <joe@infusion-it.co.uk>
2019-01-24 18:56:01 +0000

    add changes from server (automatic commit)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -85,7 +85,7 @@
 							@slot('name', 'Jesus Gonzalez Nieto')
 							@slot('role', 'Software Developer')
 							@slot('bio')
-Jesus has a BSc in Computer Science, a Master's in Decentralized Computing and 4 years of experience as a programmer
+Jesus has a BSc in Computer Science, a Master's Degree in Decentralized Computing. He also counts with over 4 years experience as a programmer, and is a core developer here at Internxt.
 							@endslot
 						@endcomponent
 					</div>{{-- /.col-lg-3 --}}
