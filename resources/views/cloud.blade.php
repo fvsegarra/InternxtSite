8f7144d4e4a7317145759cde9cf6328a0b1fc071
Joe Priest <joe@infusion-it.co.uk>
2019-02-06 14:16:01 +0000

    add changes from server (automatic commit)

--- resources/views/cloud.blade.php
+++ resources/views/cloud.blade.php
@@ -18,7 +18,7 @@
 			<h1 class="pageHeader__heading">X Cloud</h1>
 			<h2 class="pageHeader__subheading">Secure cloud storage.</h2>
 			<a href="#" class="pageHeader__cta pageHeader__cta--mobile">Get X Cloud Beta</a>
-			<a href="https://cloud.internxt.com/" target='_blank' class="pageHeader__cta pageHeader__cta--desktop" data-toggle="modal" data-target="#modalXCloud">Get X Cloud Beta</a>
+			<a href="https://cloud.internxt.com/" target='_blank' class="pageHeader__cta pageHeader__cta--desktop">Get X Cloud Beta</a>
 		</div>
 	</header>
 
