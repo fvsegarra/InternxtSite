97ca1444abc712f75696ba5e50b6dd6cfec8f4e2
Joe Priest <joe@infusion-it.co.uk>
2019-02-21 09:58:01 +0000

    add changes from server (automatic commit)

--- resources/views/token.blade.php
+++ resources/views/token.blade.php
@@ -26,7 +26,7 @@
 				<div class="col-3">
 					<div class="inxtDatum">
 						<p class="inxtDatum__label">Price Per INXT</p>
-						<p class="inxtDatum__point">€<span id="usd_price">1.40</span></p>
+						<p class="inxtDatum__point">€<span id="usd_price">1.5</span></p>
 					</div>{{-- /.inxtDatum --}}
 				</div>{{-- /.col-3 --}}
 
