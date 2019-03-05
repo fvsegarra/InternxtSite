cc49584c2f7cadac1767d387fd9472a9304b6975
Joe Priest <joe@infusion-it.co.uk>
2019-03-05 16:11:01 +0000

    add changes from server (automatic commit)

--- resources/views/_includes/navbar-mobile.blade.php
+++ resources/views/_includes/navbar-mobile.blade.php
@@ -12,6 +12,7 @@
         <li class="menu-item"><a href="{{ route('cloud') }}">X Cloud</a></li>
         <li class="menu-item"><a href="{{ route('core') }}">X Core</a></li>
         <li class="menu-item"><a href="{{ route('about') }}">About Us</a></li>
+        <li class="menu-item"><a href="{{ route('roadmap') }}">Roadmap</a></li>
         <li class="menu-item"><a href="{{ route('merchandise') }}">Merchandise</a></li>
         <li class="menu-item"><a href="{{ route('token') }}">INXT token</a></li>
         <li class="menu-item"><a href="https://medium.com/internxt" target="_blank" rel="nofollow">News hub</a></li>
