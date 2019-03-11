487b910d3a5137bf15cf37fc84db4f6d53842af3
Joe Priest <joe@infusion-it.co.uk>
2019-03-11 15:14:01 +0000

    add changes from server (automatic commit)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,7 +19,7 @@
 
 			<div class="memberProfile__content row align-items-center">
 				<div class="col-lg-3">
-					<img src="/img/team/fran.png" alt="Fran" class="memberProfile__image img-fluid">
+					<img src="/img/team/fran.png" alt="Fran Villalba Segarra" class="memberProfile__image img-fluid">
 				</div>
 				<div class="col-lg-9 text-left">
 					<p class="memberProfile__name">Fran Villalba Segarra</p>
