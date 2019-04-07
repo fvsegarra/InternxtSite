4c83f1bb43e4893e602bad1f38c39348b47bdae5
Joe Priest <joe@infusion-it.co.uk>
2019-04-07 15:20:01 +0000

    add changes from server (automatic commit)

--- resources/views/cloud.blade.php
+++ resources/views/cloud.blade.php
@@ -85,7 +85,7 @@
 			</ul>
 
 			<div class="tab-content" id="xcloudFeaturesTabContent">
-				<div class="tab-pane fade show active" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab"><em>A more limitless workflow.</em> To make using your cloud easier, your file options are spread across the dashboard so you can get where you want to be without having to click multiple times.</div>
+				<div class="tab-pane fade show active" id="dashboard" role="tabpanel" aria-labelledby="dashboard-tab"><em>A more limitless workflow.</em> To make using your cloud easier, your file options are spread across the dashboard so you can get where you want to be without too many clicks.</div>
 				<div class="tab-pane fade" id="folders" role="tabpanel" aria-labelledby="folders-tab"><em>Folders can be customised in various colours.</em> Maybe you prefer a Health folder green or a Movie folder purple, to increase visibility, to suit the contents theme or to just simply look more appealing. You can also add a variety of different icons on the cover of any folder. Maybe you want to add a Dollar icon to a salary folder or an image icon to your holiday folder.</div>
 			</div>
 
