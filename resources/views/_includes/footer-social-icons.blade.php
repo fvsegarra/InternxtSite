b6a7f313b5ac7cb9857aa653214721e7c0245536
Joe Priest <joe@infusion-it.co.uk>
2019-05-13 08:21:01 +0000

    add changes from server (automatic commit)

--- resources/views/_includes/footer-social-icons.blade.php
+++ resources/views/_includes/footer-social-icons.blade.php
@@ -2,7 +2,7 @@
     <a class="nav-link" rel="nofollow" target="_blank" href="https://twitter.com/Internxt" rel="noopener"><img src="/img/logos/twitter.png" height="22"></a>
 </li>
 <li class="nav-item nav-item--social">
-    <a class="nav-link" rel="nofollow" target="_blank" href="https://medium.com/internxt" rel="noopener"><img src="/img/logos/medium.png" height="22"></a>
+    <a class="nav-link" rel="nofollow" target="_blank" href="https://medium.com/Internxt" rel="noopener"><img src="/img/logos/medium.png" height="22"></a>
 </li>
 <li class="nav-item nav-item--social">
     <a class="nav-link" rel="nofollow" target="_blank" href="https://github.com/Internxt/" rel="noopener"><img src="/img/logos/github.png" height="22"></a>
@@ -11,8 +11,8 @@
     <a class="nav-link" rel="nofollow" target="_blank" href="mailto:hello@internxt.com" rel="noopener"><img src="/img/logos/mail.png" height="21"></a>
 </li>
 <li class="nav-item nav-item--social">
-    <a class="nav-link" rel="nofollow" target="_blank" href="tg://resolve?domain=InternxtHQ" rel="noopener"><img src="/img/logos/telegram.png" height="21"></a>
+    <a class="nav-link" rel="nofollow" target="_blank" href="tg://resolve?domain=WeAreInternxt" rel="noopener"><img src="/img/logos/telegram.png" height="21"></a>
 </li>
 <li class="nav-item nav-item--social">
-    <a class="nav-link" rel="nofollow" target="_blank" href="https://www.instagram.com/InternxtHQ/" rel="noopener"><img src="/img/logos/instagram.png" height="22"></a>
+    <a class="nav-link" rel="nofollow" target="_blank" href="https://www.instagram.com/WeAreInternxt/" rel="noopener"><img src="/img/logos/instagram.png" height="22"></a>
 </li>
