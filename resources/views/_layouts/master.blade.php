e32f9b254268061f9afc873ca57641f5e9da6012
Joe Priest <joe@infusion-it.co.uk>
2019-05-27 17:29:01 +0000

    add changes from server (automatic commit)

--- resources/views/_layouts/master.blade.php
+++ resources/views/_layouts/master.blade.php
@@ -114,7 +114,7 @@
                                     <a class="nav-link nav-link--social nav-link--twitter" href="https://twitter.com/Internxt" target="_blank" rel="noopener"><img src="/img/logos/twitter.png" height="25"></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link nav-link--social nav-link--medium" href="https://medium.com/internxt" target="_blank" rel="noopener"><img src="/img/logos/medium.png" height="25"></a>
+                                    <a class="nav-link nav-link--social nav-link--medium" href="https://medium.com/Internxt" target="_blank" rel="noopener"><img src="/img/logos/medium.png" height="25"></a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link nav-link--social nav-link--github" href="https://github.com/Internxt" target="_blank" rel="noopener"><img src="/img/logos/github.png" height="25"></a>
@@ -123,10 +123,10 @@
                                     <a class="nav-link nav-link--social nav-link--mail" href="mailto:hello@internxt.com"><img src="/img/logos/mail.png" height="25"></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link nav-link--social nav-link--telegram" href="https://t.me/InternxtHQ" target="_blank" rel="noopener"><img src="/img/logos/telegram.png" height="25"></a>
+                                    <a class="nav-link nav-link--social nav-link--telegram" href="https://t.me/WeAreInternxt" target="_blank" rel="noopener"><img src="/img/logos/telegram.png" height="25"></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link nav-link--social nav-link--instagram" href="https://www.instagram.com/InternxtHQ/" target="_blank" rel="noopener"><img src="/img/logos/instagram.png" height="25"></a>
+                                    <a class="nav-link nav-link--social nav-link--instagram" href="https://www.instagram.com/Internxt/" target="_blank" rel="noopener"><img src="/img/logos/instagram.png" height="25"></a>
                                </li>
                             </ul>
 
